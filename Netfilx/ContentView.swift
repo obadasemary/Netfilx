0fe66c92b587433ac1530c3de9e36a9129936a28
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-08-31 17:03:17 +0300

    Initial Commit

---
+++ Netfilx/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Netfilx
+//
+//  Created by Abdelrahman Mohamed on 31.08.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Netfilx/NetfilxApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetfilxApp.swift
+//  Netfilx
+//
+//  Created by Abdelrahman Mohamed on 31.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct NetfilxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
