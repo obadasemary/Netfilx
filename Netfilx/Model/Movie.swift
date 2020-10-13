4196d15be35765210a78ed970560fca48d36ec02
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-10-13 06:32:11 +0300

    Update Movie.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- Netfilx/Model/Movie.swift
+++ Netfilx/Model/Movie.swift
@@ -20,4 +20,47 @@ struct Movie: Identifiable {
     var year: Int
     var rating: String
     var numberOfSeasons: Int?
+    
+    // MARK: - Personalization
+    
+    var currentEpisodeInfo: CurrentEpisodeInfo?
+    var defaultEpisodeInfo: CurrentEpisodeInfo
+    
+    var creators: String
+    var cast: String
+    
+    var episode: [Episode]?
+    
+    var promotionHeadline: String?
+    
+    var numberOfSeasonsDisplay: String {
+        
+        if let num = numberOfSeasons {
+            if num == 1 {
+                return "1 Season"
+            } else {
+                return "\(num) Seasons"
+            }
+        }
+        
+        return ""
+    }
+    
+    var episodeInfoDisplay: String {
+        
+        if let current = currentEpisodeInfo {
+            return "S\(current.season): E\(current.episode) \(current.episodeName)"
+        } else {
+            return "S\(defaultEpisodeInfo.season): E\(defaultEpisodeInfo.episode) \(defaultEpisodeInfo.episodeName)"
+        }
+    }
+    
+    var episodeDescriptionDisplay: String {
+        
+        if let current = currentEpisodeInfo {
+            return current.description
+        } else {
+            return defaultEpisodeInfo.description
+        }
+    }
 }
