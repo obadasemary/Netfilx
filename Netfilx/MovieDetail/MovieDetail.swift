9f22839134e626ed843c9d116f46adc712e653de
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-10-13 03:36:47 +0300

    Create MovieDetail.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ Netfilx/MovieDetail/MovieDetail.swift
@@ -0,0 +1,77 @@
+//
+//  MovieDetail.swift
+//  Netfilx
+//
+//  Created by Abdelrahman Mohamed on 13.10.2020.
+//
+
+import SwiftUI
+
+struct MovieDetail: View {
+    
+    var movie: Movie
+    let screen = UIScreen.main.bounds
+    
+    var body: some View {
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                HStack {
+                    Spacer()
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        Image(systemName: "xmark.circle")
+                            .font(.system(size: 28))
+                    })
+                }
+                .padding(.horizontal, 22)
+                
+                ScrollView(.vertical, showsIndicators: false) {
+                    
+                    VStack {
+                        
+                        StandarHomeMovie(movie: movie)
+                            .frame(width: screen.width / 2.5)
+                        
+                        MovieInfoSubheadline(movie: movie)
+                    }
+                }
+                
+                Spacer()
+            }
+            .foregroundColor(.white)
+        }
+    }
+}
+
+struct MovieDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieDetail(movie: exampleMovie3)
+    }
+}
+
+struct MovieInfoSubheadline: View {
+    
+    var movie: Movie
+    
+    var body: some View {
+        
+        HStack {
+            
+            Image(systemName: "hand.thumbsup.fill")
+                .foregroundColor(.white)
+            
+            Text("MOVIE YEAR")
+            
+            Text("RATING")
+            
+            Text("seasons")
+        }
+        .foregroundColor(.gray)
+        .padding(.vertical, 6)
+    }
+}
