34547a502de63f3aa773803ccb2cc40954ce1d30
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-10-13 06:32:33 +0300

    Update TopMoviePreview.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- Netfilx/HelperViews/TopMoviePreview.swift
+++ Netfilx/HelperViews/TopMoviePreview.swift
@@ -67,7 +67,7 @@ struct TopMoviePreview: View {
                     
                     Spacer()
                     
-                    WhiteButton(text: "Play", imageName: "play.fill") {
+                    PlayButton(text: "Play", imageName: "play.fill") {
                         
                         //
                     }
