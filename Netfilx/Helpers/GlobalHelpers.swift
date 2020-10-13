170d4fb2db11338ca0b985a3036bb0d285b194f9
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-10-13 03:57:19 +0300

    Update GlobalHelpers.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- Netfilx/Helpers/GlobalHelpers.swift
+++ Netfilx/Helpers/GlobalHelpers.swift
@@ -8,23 +8,98 @@
 import Foundation
 import SwiftUI
 
-let categories = ["Action", "Adventure", "Animation", "Comedy"]
+let categories = [
+    "Action",
+    "Adventure",
+    "Animation",
+    "Comedy"
+]
 
 //    ["Action", "Adventure", "Animation", "Biography", "Comedy", "Crime", "Documentary", "Drama", "Family", "Fantasy", "Film Noir", "History", "Horror", "Music", "Musical", "Mystery", "Romance", "Sci-Fi", "Short Film", "Sport", "Superhero", "Thriller", "War", "Western"]
 
-let exampleMovie0 = Movie(id: UUID().uuidString, name: "DARK", thumbnailURL: URL(string: "https://picsum.photos/200/300")!, categories: categories)
-let exampleMovie1 = Movie(id: UUID().uuidString, name: "DARK", thumbnailURL: URL(string: "https://picsum.photos/200/301")!, categories: categories)
-let exampleMovie2 = Movie(id: UUID().uuidString, name: "DeadPool", thumbnailURL: URL(string: "https://picsum.photos/200/302")!, categories: categories)
-let exampleMovie3 = Movie(id: UUID().uuidString, name: "DeadPool 2", thumbnailURL: URL(string: "https://picsum.photos/200/303")!, categories: categories)
-let exampleMovie4 = Movie(id: UUID().uuidString, name: "Spider Man", thumbnailURL: URL(string: "https://picsum.photos/200/304")!, categories: categories)
-let exampleMovie5 = Movie(id: UUID().uuidString, name: "Dark Night", thumbnailURL: URL(string: "https://picsum.photos/200/305")!, categories: categories)
-let exampleMovie6 = Movie(id: UUID().uuidString, name: "Super Man", thumbnailURL: URL(string: "https://picsum.photos/200/306")!, categories: categories)
+let exampleMovie0 = Movie(
+    id: UUID().uuidString,
+    name: "DARK",
+    thumbnailURL: URL(string: "https://picsum.photos/200/300")!,
+    categories: categories,
+    year: 2020,
+    rating: "TV-MA"
+)
 
-let exampleMovies: [Movie] = [exampleMovie0, exampleMovie1, exampleMovie2, exampleMovie3, exampleMovie4, exampleMovie5, exampleMovie6]
+let exampleMovie1 = Movie(
+    id: UUID().uuidString,
+    name: "DARK",
+    thumbnailURL: URL(string: "https://picsum.photos/200/301")!,
+    categories: categories,
+    year: 2020,
+    rating: "TV-MA"
+)
+
+let exampleMovie2 = Movie(
+    id: UUID().uuidString,
+    name: "DeadPool",
+    thumbnailURL: URL(string: "https://picsum.photos/200/302")!,
+    categories: categories,
+    year: 2020,
+    rating: "TV-MA"
+)
+
+let exampleMovie3 = Movie(
+    id: UUID().uuidString,
+    name: "DeadPool 2",
+    thumbnailURL: URL(string: "https://picsum.photos/200/303")!,
+    categories: categories,
+    year: 2020,
+    rating: "TV-MA"
+)
+
+let exampleMovie4 = Movie(
+    id: UUID().uuidString,
+    name: "Spider Man",
+    thumbnailURL: URL(string: "https://picsum.photos/200/304")!,
+    categories: categories,
+    year: 2020,
+    rating: "TV-MA"
+)
+
+let exampleMovie5 = Movie(
+    id: UUID().uuidString,
+    name: "Dark Night",
+    thumbnailURL: URL(string: "https://picsum.photos/200/305")!,
+    categories: categories,
+    year: 2020,
+    rating: "TV-MA"
+)
+
+let exampleMovie6 = Movie(
+    id: UUID().uuidString,
+    name: "Super Man",
+    thumbnailURL: URL(string: "https://picsum.photos/200/306")!,
+    categories: categories,
+    year: 2020,
+    rating: "TV-MA"
+)
+
+let exampleMovies: [Movie] = [
+    exampleMovie0,
+    exampleMovie1,
+    exampleMovie2,
+    exampleMovie3,
+    exampleMovie4,
+    exampleMovie5,
+    exampleMovie6
+]
 
 extension LinearGradient {
- 
-    static let blackOpacityGradient = LinearGradient(gradient: Gradient(colors: [Color.black.opacity(0.0), Color.black.opacity(0.95)]), startPoint: .top, endPoint: .bottom)
+    
+    static let blackOpacityGradient = LinearGradient(
+        gradient: Gradient(
+            colors: [
+                Color.black.opacity(0.0),
+                Color.black.opacity(0.95)
+            ]
+        ),
+        startPoint: .top,
+        endPoint: .bottom
+    )
 }
-
-
