136b1e3bcdf7077c88beef22e0f0b95f8d030f09
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-10-13 03:15:47 +0300

    Create HomeVM.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ Netfilx/HomeViews/HomeVM.swift
@@ -0,0 +1,38 @@
+//
+//  HomeVM.swift
+//  Netfilx
+//
+//  Created by Abdelrahman Mohamed on 31.08.2020.
+//
+
+import Foundation
+
+class HomeVM: ObservableObject {
+    
+    // String == Category
+    @Published var movies: [String: [Movie]] = [:]
+    
+    public var allCategory: [String] {
+        movies.keys.map({ String($0) })
+    }
+    
+    public func getMovie(forCat cat: String) -> [Movie] {
+        return movies[cat] ?? []
+    }
+    
+    init() {
+        
+        setupMovies()
+    }
+    
+    func setupMovies() {
+        
+        movies["Trending Now"] = exampleMovies
+        movies["Netfilx Originals"] = exampleMovies.shuffled()
+        movies["Comedies"] = exampleMovies.shuffled()
+        movies["Animation"] = exampleMovies.shuffled()
+        movies["Egyptian Comedies"] = exampleMovies.shuffled()
+        movies["Egyptian Movies"] = exampleMovies.shuffled()
+        movies["New Releases"] = exampleMovies.shuffled()
+    }
+}
