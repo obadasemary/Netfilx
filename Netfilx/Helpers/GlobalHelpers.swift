33bd2a2b7a06326864505291cfd01cb736512c63
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-10-13 06:32:43 +0300

    Update GlobalHelpers.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- Netfilx/Helpers/GlobalHelpers.swift
+++ Netfilx/Helpers/GlobalHelpers.swift
@@ -23,7 +23,12 @@ let exampleMovie0 = Movie(
     thumbnailURL: URL(string: "https://picsum.photos/200/300")!,
     categories: categories,
     year: 2020,
-    rating: "TV-MA"
+    rating: "TV-MA",
+    numberOfSeasons: 1,
+    defaultEpisodeInfo: exampleEpisodeInfo1,
+    creators: "Abdelrahman Mohamed, Nazli Abdelrahman, Sara Abdelrahman",
+    cast: "Rami Malek, Obada Mohamed, Abdelrahman Mohamed",
+    promotionHeadline: "New Season 2 comming soon"
 )
 
 let exampleMovie1 = Movie(
@@ -32,7 +37,11 @@ let exampleMovie1 = Movie(
     thumbnailURL: URL(string: "https://picsum.photos/200/301")!,
     categories: categories,
     year: 2020,
-    rating: "TV-MA"
+    rating: "TV-MA",
+    numberOfSeasons: 1,
+    defaultEpisodeInfo: exampleEpisodeInfo1,
+    creators: "Abdelrahman Mohamed, Nazli Abdelrahman, Sara Abdelrahman",
+    cast: "Rami Malek, Obada Mohamed, Abdelrahman Mohamed"
 )
 
 let exampleMovie2 = Movie(
@@ -41,7 +50,12 @@ let exampleMovie2 = Movie(
     thumbnailURL: URL(string: "https://picsum.photos/200/302")!,
     categories: categories,
     year: 2020,
-    rating: "TV-MA"
+    rating: "TV-MA",
+    numberOfSeasons: 2,
+    defaultEpisodeInfo: exampleEpisodeInfo1,
+    creators: "Abdelrahman Mohamed, Nazli Abdelrahman, Sara Abdelrahman",
+    cast: "Rami Malek, Obada Mohamed, Abdelrahman Mohamed",
+    promotionHeadline: "Best Rated Show"
 )
 
 let exampleMovie3 = Movie(
@@ -50,7 +64,11 @@ let exampleMovie3 = Movie(
     thumbnailURL: URL(string: "https://picsum.photos/200/303")!,
     categories: categories,
     year: 2020,
-    rating: "TV-MA"
+    rating: "TV-MA",
+    numberOfSeasons: 3,
+    defaultEpisodeInfo: exampleEpisodeInfo1,
+    creators: "Abdelrahman Mohamed, Nazli Abdelrahman, Sara Abdelrahman",
+    cast: "Rami Malek, Obada Mohamed, Abdelrahman Mohamed"
 )
 
 let exampleMovie4 = Movie(
@@ -59,7 +77,12 @@ let exampleMovie4 = Movie(
     thumbnailURL: URL(string: "https://picsum.photos/200/304")!,
     categories: categories,
     year: 2020,
-    rating: "TV-MA"
+    rating: "TV-MA",
+    numberOfSeasons: 4,
+    defaultEpisodeInfo: exampleEpisodeInfo1,
+    creators: "Abdelrahman Mohamed, Nazli Abdelrahman, Sara Abdelrahman",
+    cast: "Rami Malek, Obada Mohamed, Abdelrahman Mohamed",
+    promotionHeadline: "New Episodes coming soon"
 )
 
 let exampleMovie5 = Movie(
@@ -68,7 +91,11 @@ let exampleMovie5 = Movie(
     thumbnailURL: URL(string: "https://picsum.photos/200/305")!,
     categories: categories,
     year: 2020,
-    rating: "TV-MA"
+    rating: "TV-MA",
+    numberOfSeasons: 5,
+    defaultEpisodeInfo: exampleEpisodeInfo1,
+    creators: "Abdelrahman Mohamed, Nazli Abdelrahman, Sara Abdelrahman",
+    cast: "Rami Malek, Obada Mohamed, Abdelrahman Mohamed"
 )
 
 let exampleMovie6 = Movie(
@@ -77,7 +104,12 @@ let exampleMovie6 = Movie(
     thumbnailURL: URL(string: "https://picsum.photos/200/306")!,
     categories: categories,
     year: 2020,
-    rating: "TV-MA"
+    rating: "TV-MA",
+    numberOfSeasons: 6,
+    defaultEpisodeInfo: exampleEpisodeInfo1,
+    creators: "Abdelrahman Mohamed, Nazli Abdelrahman, Sara Abdelrahman",
+    cast: "Rami Malek, Obada Mohamed, Abdelrahman Mohamed",
+    promotionHeadline: "Watch Season 6 Now"
 )
 
 let exampleMovies: [Movie] = [
@@ -89,6 +121,13 @@ let exampleMovies: [Movie] = [
     exampleMovie5,
     exampleMovie6
 ]
+
+let exampleEpisodeInfo1 = CurrentEpisodeInfo(
+    episodeName: "Hello, Elliot",
+    description: "Elliot gets to Coney Island, and the delusion begins to slip. Mr. Robot reveals the truth.",
+    season: 2,
+    episode: 1
+)
 
 extension LinearGradient {
     
